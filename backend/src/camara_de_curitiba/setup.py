fdd2e3c691cc59fb0a85ea7a31a88ebb84d0892c
Bruno Oliveira <bruno.oliveira@cmc.pr.gov.br>
2025-07-01 12:01:45 -0300

    bump b 0.0.2

--- backend/src/camara_de_curitiba/setup.py
+++ backend/src/camara_de_curitiba/setup.py
@@ -14,7 +14,7 @@ long_description = f"""
 
 setup(
     name="camara_de_curitiba",
-    version="1.0.0",
+    version="0.0.2",
     description="Câmara de Curitiba configuration package.",
     long_description=long_description,
     classifiers=[
